dd291104dada61e1d6670660198765af058c1569
Kohl <kohlbyrd@Jamies-MacBook-Air.local>
2021-09-16 21:28:58 -0500

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#messageOne')
+const messageTwo = document.querySelector('#messageTwo')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const location = search.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((response)=> {
+        response.json()
+            .then((data) => {
+                if(data.error) {
+                     console.log(data.error)
+                     messageTwo.textContent = data.error
+                } else {
+                    console.log(data.address)
+                    console.log(data.forcast)
+                    console.log(data.temp)
+                    messageOne.textContent = data.forcast +', ' + data.temp + ' degrees'
+                }
+                
+            })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=d583db3d0bf9784242ef56ff566e718f&query=' + lat +','+ long + '&units=f'
+    request({url, json: true}, (error, { body }) => {
+       if(error) {
+           callback('ERROR',undefined)
+       } else if (body.error) {
+            callback('UNABLE TO FIND LOCATION (forecast.js)', error)
+       } else {
+            callback(undefined, [body.current.weather_descriptions[0], body.current.temperature])
+       }
+    })
+} 
+
+module.exports = forecast;
